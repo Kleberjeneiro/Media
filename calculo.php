57def295f601900611128fad3f091322d0204825
Kleber Jeneiro Filho <100257551+Kleberjeneiro@users.noreply.github.com>
2023-08-31 22:17:51 -0300

    Update calculo.php
    Criação da Função somaResultado

--- calculo.php
+++ calculo.php
@@ -22,143 +22,66 @@
                 <h1></h1>
 
                 <?php
-
-                    $Escolha = $_POST["Escolha"];
-                    $nota1 = $_POST["nota1"];
-                    $nota2 = $_POST["nota2"];
-                    $nota3 = $_POST["nota3"];
-                    $nota4 = $_POST["nota4"];
-
                     
+                    $Escolha = $_POST["Escolha"];
+
+                    function somaResultado() {
+
+                        $Escolha = $_POST["Escolha"];
+                        $nota1 = $_POST["nota1"];
+                        $nota2 = $_POST["nota2"];
+                        $nota3 = $_POST["nota3"];
+                        $nota4 = $_POST["nota4"];
+                        $media = $_POST["media"];
+                        $resul = ($nota1 + $nota2 + $nota3 + $nota4) / 4;
+
+                        if ( $resul >= $media) {
+
+                            echo "Parabéns você foi Aprovado $Escolha: nota $resul";
+
+                        } elseif ($resul < $media) {
+
+                            echo "Você está de recuperação $Escolha: nota = $resul";
+                        }
+                    } 
 
                     switch ($Escolha) {
-                        case 'Biologia':
+                        case "Biologia":
                             
-                            $media = 20;
-                            $resul = ($nota1 + $nota2 + $nota3 + $nota4) / 4;
-
-                            if ( $resul >= $media) {
-
-                                echo "Parabéns você foi Aprovado em Biologia: nota $resul";
-
-                            } else {
-
-                                echo "Você está de recuperação em Biologia: nota = $resul";
-                            }
-                            break;
-                            case 'Filosofia':
+                            somaResultado();
+                        break;
+                        case 'Filosofia':
                             
-                                $media = 20;
-                                $resul = ($nota1 + $nota2 + $nota3 + $nota4) / 4;
-        
-                                if ( $resul >= $media) {
-        
-                                    echo "Parabéns você foi Aprovado em Filosofia: $resul";
-        
-                                } else {
-        
-                                    echo "Você está de recuperação em Filosofia: $resul";
-                                } 
-                                
-                                break;
-                                case 'Geografia':
+                            somaResultado(); 
+                        break;
+                        case 'Geografia':
                             
-                                    $media = 20;
-                                    $resul = ($nota1 + $nota2 + $nota3 + $nota4) / 4;
-            
-                                    if ( $resul >= $media) {
-            
-                                        echo "Parabéns você foi Aprovado em Geografia : $resul";
-            
-                                    } else {
-            
-                                        echo "Você está de recuperação em Geografia : $resul";
-                                    }
-                                    break;
-                                case 'História':
+                            somaResultado(); 
+                        break;
+                        case 'História':
                                     
-                                    $media = 25;
-                                    $resul = ($nota1 + $nota2 + $nota3 + $nota4) / 4;
-            
-                                    if ( $resul >= $media) {
-            
-                                        echo "Parabéns você foi Aprovado em História : $resul";
-            
-                                    } else {
-            
-                                        echo "Você está de recuperação em História : $resul";
-                                    }
-                                    break;
-                                    case 'Língua Inglesa':
+                            somaResultado(); 
+                        break;
+                        case 'Língua Inglesa':
                                     
-                                        $media = 30;
-                                        $resul = ($nota1 + $nota2 + $nota3 + $nota4) / 4;
-                
-                                        if ( $resul >= $media) {
-                
-                                            echo "Parabéns você foi Aprovado em Língua Inglesa: $resul";
-                
-                                        } else {
-                
-                                            echo "Você está de recuperação em Língua Inglesa : $resul";
-                                        }
-                                        break;
-                                        case 'Língua Portuguesa':
+                            somaResultado(); 
+                        break;
+                        case 'Língua Portuguesa':
                                     
-                                            $media = 40;
-                                            $resul = ($nota1 + $nota2 + $nota3 + $nota4) / 4;
-                    
-                                            if ( $resul >= $media) {
-                    
-                                                echo "Parabéns você foi Aprovado em Língua Portuguesa : $resul";
-                    
-                                            } else {
-                    
-                                                echo "Você está de recuperação em Língua Portuguesa : $resul";
-                                            }
-                                            break;   
-                                            case 'Matemática':
+                            somaResultado(); 
+                         break;   
+                        case 'Matemática':
                                     
-                                                $media = 40;
-                                                $resul = ($nota1 + $nota2 + $nota3 + $nota4) / 4;
-                        
-                                                if ( $resul >= $media) {
-                        
-                                                    echo "Parabéns você foi Aprovado em Matemática : $resul";
-                        
-                                                } else {
-                        
-                                                    echo "Você está de recuperação em Matemática : $resul";
-                                                }
-                                                break;
-                                                case 'Química':
+                            somaResultado(); 
+                        break;
+                        case 'Química':
                                     
-                                                    $media = 35;
-                                                    $resul = ($nota1 + $nota2 + $nota3 + $nota4) / 4;
-                            
-                                                    if ( $resul >= $media) {
-                            
-                                                        echo "Parabéns você foi Aprovado em Química : $resul";
-                            
-                                                    } else {
-                            
-                                                        echo "Você está de recuperação em Química : $resul";
-                                                    }
-                                                    break;
-                                                    case 'Sociologia':
+                            somaResultado(); 
+                        break;
+                        case 'Sociologia':
                                     
-                                                        $media = 30;
-                                                        $resul = ($nota1 + $nota2 + $nota3 + $nota4) / 4;
-                                
-                                                        if ( $resul >= $media) {
-                                
-                                                            echo "Parabéns você foi Aprovado em Sociologia : $resul";
-                                
-                                                        } else {
-                                
-                                                            echo "Você está de recuperação em Sociologia : $resul";
-                                                        }
-                                                        break;                 
+                            somaResultado(); 
+                        break;                 
                         default:
                             # code...
                         break;
@@ -176,6 +99,4 @@
     </body>
 
 </html>
-
-
 
